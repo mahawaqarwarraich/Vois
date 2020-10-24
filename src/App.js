9b34007dc01ca6d772c9e71eb6bb4432c1b4173d
Haysam Bin Tahir <haysam.bin.tahir@gmail.com>
2020-10-25 01:25:27 +0500

    Text editor basic functionality with basic styling added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
 
 // import HomePage from "./containers/Pages/HomePage";
 // import ArticleTopicCard from "./components/ArticlesDirectory/ArticleTopicCard/ArticleTopicCard";
@@ -6,22 +7,40 @@ import React from 'react';
 // import ArticleMainHeader from "./components/ArticlesDirectory/ArticleMainHeader/ArticleMainHeader";
 // import SearchBar from "./components/ArticlesDirectory/UIElements/SearchBar/SearchBar";
 // import ArticleTopics from "./containers/ArticlesDirectory/ArticleTopics/ArticleTopics";
-import ArticleTopicsPage from "./containers/Pages/ArticleTopicsPage/ArticleTopicsPage";
+// import ArticleTopicsPage from "./containers/Pages/ArticleTopicsPage/ArticleTopicsPage";
+import TextEditor from "./containers/TextEditor";
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {/*<HomePage />*/}
-      {/*<ArticleTopicCard />*/}
-      {/*<ArticleListItem />*/}
-      {/*<ArticleMainHeader />*/}
-      {/*<SearchBar />*/}
-      {/*<ArticleTopics />*/}
-      <ArticleTopicsPage />
-    </div>
-  );
+
+    const [boldState, setBoldState] = useState(false);
+
+
+
+
+    useEffect(() => {
+        SpeechRecognition.startListening({continuous: true});
+    }, [])
+
+
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        return null
+    }
+
+    return (
+        <div className="App">
+            {/*<HomePage />*/}
+            {/*<ArticleTopicCard />*/}
+            {/*<ArticleListItem />*/}
+            {/*<ArticleMainHeader />*/}
+            {/*<SearchBar />*/}
+            {/*<ArticleTopics />*/}
+            {/*<ArticleTopicsPage />*/}
+            <TextEditor/>
+
+        </div>
+    );
 }
 
 export default App;
